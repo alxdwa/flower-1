fb6925db8ca27ce9783e24f6378a16aeb2d265e9
Alex Diwa <atdiwa@gmail.com>
2019-03-03 19:18:02 +1100

    modified colors of flower

--- sketch.js
+++ sketch.js
@@ -2,29 +2,32 @@ let x;
 let y;
 let r;
 let off;
+let col = 61;
 
 function setup() {
-	createCanvas(600, 700);
+	createCanvas(600, 600);
 	angleMode(DEGREES);
+	colorMode(HSL);
 }
 
 function draw() {
-	background(233);
+	background(233, 0, 88);
 	strokeWeight(0.5);
 	noFill();
 	push();
 	translate(width/2, height/2);
-	stroke(0, 0, 255);
+
 
 	for (let j = 0; j < 100; j++) {
-		off = j*2
+		stroke(132 + (j * 1), 90, 61);
+		off = j * 2;
 
 		beginShape();
 		for (let i = 0; i < 360; i++) {
-			r = map(noise(sin(i)*frameCount/100), 0, 1, 0+off, 100+off)
+			r = map(noise(sin(i) * (frameCount/100)), 0, 1, 0 + off, 80 + off);
 			x = r * cos(i) * sin(j);
 			y = r * sin(i);
-			vertex(x,y);
+			vertex(x, y);
 		}
 		endShape(CLOSE);
 	}
