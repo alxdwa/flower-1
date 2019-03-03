a0b8fde31f0956523ec470f5664fc51ca05d2c9e
Alex Diwa <atdiwa@gmail.com>
2019-03-03 19:58:21 +1100

    added CCapture.js functionality for recording gif and modified frameCount

--- sketch.js
+++ sketch.js
@@ -2,10 +2,17 @@ let x;
 let y;
 let r;
 let off;
-let col = 61;
+// let gifLength = 360;
+// let canvas;
+
 
 function setup() {
+	// var p5Canvas = 
 	createCanvas(600, 600);
+	// canvas = p5Canvas.canvas;
+
+	// capturer.start();
+
 	angleMode(DEGREES);
 	colorMode(HSL);
 }
@@ -17,14 +24,13 @@ function draw() {
 	push();
 	translate(width/2, height/2);
 
-
-	for (let j = 0; j < 100; j++) {
-		stroke(132 + (j * 1), 90, 61);
+	for (let j = 0; j < 100; j += 1) {
+		stroke(132 + j, 90, 61);
 		off = j * 2;
 
 		beginShape();
 		for (let i = 0; i < 360; i++) {
-			r = map(noise(sin(i) * (frameCount/100)), 0, 1, 0 + off, 80 + off);
+			r = map(noise(sin(i) * (frameCount/20)), 0, 1, 0 + off, 80 + off);
 			x = r * cos(i) * sin(j);
 			y = r * sin(i);
 			vertex(x, y);
@@ -32,10 +38,17 @@ function draw() {
 		endShape(CLOSE);
 	}
 	pop();
+
+	// if (frameCount < gifLength) {
+	// 		capturer.capture(canvas);
+	// } else if (frameCount === gifLength) {
+	// 		capturer.stop();
+	// 		capturer.save();
+	// }
 }
 
 //	r = map(noise(sin(i)+(frameCount/100)), 0, 1, 100, 300)
-// this makes blob exterior evolve, but does not fold upon itself
+//this makes blob exterior evolve, but it does not fold upon itself
 
 //r = map(noise(sin(i)*(frameCount/100)), 0, 1, 100, 300)
 //exterior folds in on itself in vertical direction
@@ -44,4 +57,4 @@ function draw() {
 //exterior evolves as a flower, noise emerging from center 
 
 //r = map(noise(sin(i)*frameCount/100), 0, 1, 0, 100+off)
-//evolves with more noise around exterior
+//as above, but evolves with more noise around exterior
